4b974edeb592cb0c851997fd5516456f8e01de30
Lai Anh <laingocanh1990@gmail.com>
2025-10-11 12:36:31 +0700

    Creating the local file resource
    Signed-off-by: Lai Anh <laingocanh1990@gmail.com>

---
+++ chapter2/listing2.1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "local_file" "literature" {
+  filename = "art_of_war.txt"
+  content = <<-EOT
+    Sun Tzu said: The art of war is of vital importance to the State.
+
+    It is a matter of life and death, a road either to safety or to
+    ruin. Hence it is a subject of inquiry which can on no account be
+    neglected.
+
+    TBC...
+  EOT
+}
