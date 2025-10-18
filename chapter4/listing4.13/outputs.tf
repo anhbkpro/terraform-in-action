480cc644d39500efe3694600dd7a79cde3327896
Lai Anh <laingocanh1990@gmail.com>
2025-10-18 09:46:11 +0700

    output db config
    Signed-off-by: Lai Anh <laingocanh1990@gmail.com>

---
+++ chapter4/listing4.13/outputs.tf
@@ -0,0 +1,10 @@
+output "db_config" {
+  value = {
+    user = aws_db_instance.database.username
+    password = random_password.password.result
+    database = aws_db_instance.database.name
+    hostname = aws_db_instance.database.address
+    port = aws_db_instance.database.port
+
+  }
+}
