be775a8366e6f83dfe6ad3751805b4cbc945b86f
Lai Anh <laingocanh1990@gmail.com>
2025-10-18 11:49:30 +0700

    trickle down data into the autoscaling module
    Signed-off-by: Lai Anh <laingocanh1990@gmail.com>

---
+++ chapter4/listing4.15/main.tf
@@ -0,0 +1,24 @@
+# main.tf in the root module
+
+module "autoscaling" {
+  source    = "./modules/autoscaling"
+  namespace = var.namespace
+  ssh_keypair = var.ssh_key_name
+
+  vpc = module.networking.vpc
+  sg  = module.networking.sg
+  db_config = module.database.db_config
+}
+
+module "database" {
+  source    = "./modules/database"
+  namespace = var.namespace
+
+  vpc = module.networking.vpc
+  sg  = module.networking.sg
+}
+
+module "networking" {
+  source    = "./modules/networking"
+  namespace = var.namespace
+}
