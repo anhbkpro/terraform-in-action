266b48779b36a59d6f682acbe27bebc0b199f810
Lai Anh <laingocanh1990@gmail.com>
2025-10-25 09:58:12 +0700

    generate the SSH key pair that use to configure the EC2 instance
    Signed-off-by: Lai Anh <laingocanh1990@gmail.com>

---
+++ chapter9/listing9.4/main.tf
@@ -0,0 +1,15 @@
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  filename = "${path.module}/ansible-key.pem"
+  content  = tls_private_key.key.private_key_pem
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "ansible-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
